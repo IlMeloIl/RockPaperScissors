02dc57f8c9f51952ae8673ece51c604a350aaafc
IlMeloIl <jgvmelo123@gmail.com>
2024-09-23 11:41:24 -0300

    Create getComputerChoice functopn

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice(max){
+    let choice = Math.floor(Math.random()*max);
+    if (choice === 0)
+        return "rock"
+    else if (choice === 1)
+        return "paper"
+    else
+        return "scissors"
+
+}
+
+let a = getComputerChoice(3);
+console.log(a);
